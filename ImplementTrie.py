d1716289b4256d1c2f8d4ab1ec71f7cce7388f91
Prutha <122393647+pruthajoshi99@users.noreply.github.com>
2025-02-26 15:27:58 -0800

    Create ImplementTrie.py

---
+++ ImplementTrie.py
@@ -0,0 +1,46 @@
+# TC - O(L) that is length of word -> For each function
+# SC  - O(N*L) for each words there are unique characters so for inserting this is would be the complexity
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.is_last = True        
+        
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.is_last        
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return True        
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_last = False
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
